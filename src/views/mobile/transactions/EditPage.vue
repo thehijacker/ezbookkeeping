da7f166f39db3a99f4a28fe43ee5f45cd9553ac7
thehijacker <thehijacker@gmail.com>
2026-01-12 15:54:31 +0100

    fix for camera capture on Android phone

--- src/views/mobile/transactions/EditPage.vue
+++ src/views/mobile/transactions/EditPage.vue
@@ -478,7 +478,7 @@
         <f7-photo-browser ref="pictureBrowser" type="popup" navbar-of-text="/"
                           :navbar-show-count="true" :exposition="false"
                           :photos="transactionPictures" :thumbs="transactionThumbs" />
-        <input ref="pictureInput" type="file" style="display: none" :accept="`${SUPPORTED_IMAGE_EXTENSIONS};capture=camera`" @change="uploadPicture($event)" />
+        <input ref="pictureInput" type="file" style="display: none" :accept="`${SUPPORTED_IMAGE_EXTENSIONS},.pdf`" @change="uploadPicture($event)" />
     </f7-page>
 </template>
 
